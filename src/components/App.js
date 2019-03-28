cd46074fe0280d6b909188e1fa34ea68c15eab31
Marilyn <yml.100205@gmail.com>
2019-03-28 03:11:20 -0400

    NarBar skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { TopBar } from './TopBar';
+import { NavBar } from './NavBar';
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <TopBar />
+        <NavBar />
       </div>
     );
   }
