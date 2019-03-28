b44f2b424a36e5fb51c3ec0005c340ae4f3195a5
Marilyn <yml.100205@gmail.com>
2019-03-28 01:09:09 -0400

    App-logo App-name complete

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { readdirSync } from 'fs';
+
 
 export class TopBar extends React.Component {
     render() {
         return (
-            <div>TopBar</div>
+            <header className="App-header">
+                <img className="App-logo" src={process.env.PUBLIC_URL + '/logo.png'} alt="logo" />
+                <h1 className="App-name-Bo"><span className="App-name-liver">Bo</span>liver</h1>
+
+                <p className="App-slogan">
+                    You pick the location,
+                    we will take care of the rest
+                </p>
+
+                <p className="App-introduction">
+                    I'm a paragraph. Click here to add your own text and edit me. 
+                    I’m a great place for you to tell a story and let your users 
+                    know a little more about you.
+                </p>
+            </header>
         )
     }
 }
