d87199564f06fb1bd735c5c847821379cfe99588
Marilyn <yml.100205@gmail.com>
2019-03-27 23:32:32 -0400

    TopBar skeleton

---
+++ src/components/TopBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { readdirSync } from 'fs';
+
+export class TopBar extends React.Component {
+    render() {
+        return (
+            <div>TopBar</div>
+        )
+    }
+}
