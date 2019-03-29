28f77eaf0be6e4563d2d04d451c1418489850319
Marilyn <yml.100205@gmail.com>
2019-03-29 15:12:12 -0400

    Login Home skeleton

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Register } from './Register';
-
+import { Login } from './Login';
+import { Home } from './Home';
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
                 <Register />
+                <Login />
+                <Home />
             </div>
         )
     }
