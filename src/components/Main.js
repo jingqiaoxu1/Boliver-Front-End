27ba45f50fe19d6fe059eaa9c34cdb15bb8a1998
Marilyn <yml.100205@gmail.com>
2019-03-29 17:38:13 -0400

    routing part1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Register } from './Register';
 import { Login } from './Login';
 import { Home } from './Home';
+import { Link, Switch, Route } from 'react-router-dom';
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Register />
-                <Login />
-                <Home />
+                <Switch>
+                    <Route path="/register" component={Register} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/home" component={Home} />
+                    <Route component={Login} />
+                </Switch>
+
+                <Link to="/register">Register</Link><br />
+                <Link to="/login">Login</Link><br />
+                <Link to="/home">Home</Link>
             </div>
         )
     }
